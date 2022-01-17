e600339ef94dc21d0db863f17bc99acb9ff57814
zartabkhalil <zartabkhalil111@gmail.com>
2022-01-17 21:19:02 +0500

    body_class and wp_body_open added to header file

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head()?>
 </head>
-<body>
+<body <?php body_class(  )?> >
+    <?php wp_body_open(  )?>
     
     <header><h1>I am header</h1></header>
    
