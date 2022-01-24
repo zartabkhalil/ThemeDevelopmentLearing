f914a62dcba6ee03ee8859c540f45cd420ffd48e
zartabkhalil <zartabkhalil111@gmail.com>
2022-01-24 23:05:29 +0500

    custom logo theme support

--- inc/classes/aquila-class.php
+++ inc/classes/aquila-class.php
@@ -6,21 +6,31 @@ require AQUILA_DIR_PATH.'/inc/classes/aquila-assets.php';
 
 use  Aqlia_Theme\Inc\Traits\Singleton;
 
- class Aquila_Theme_class{
-    use Singleton;
-    protected function __construct(){
-        Assets::get_instance();
-        $this->set_hooks();
-    }
+class Aquila_Theme_class{
+      use Singleton;
+      protected function __construct(){
+            Assets::get_instance();
+            $this->set_hooks();
+            
+	}
 
 
-    protected function set_hooks(){
-        add_action( 'after_setup_theme',[$this,'setup_theme']);
-    }
-    
-    public function setup_theme(){
-        add_theme_support( 'title-tag' );
-    }
+	protected function set_hooks(){
+            add_action( 'after_setup_theme',[$this,'setup_theme']);
+	}
+
+	public function setup_theme(){
+            add_theme_support( 'title-tag' );
+      
+            add_theme_support( 'custom-logo', [
+                  'header-text'     =>  ['site-tile','site-description'],
+                  'height'          =>  100,
+                  'width'           =>   200,
+                  'flex-width'      =>  true,
+                  'flex-height'     => true,
+                  
+                  ] );
+	}
 
 
 
