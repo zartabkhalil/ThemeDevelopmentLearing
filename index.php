0946cf326842469cbb8da5ceff04cb29159f8115
zartabkhalil <zartabkhalil111@gmail.com>
2022-01-30 15:32:33 +0500

    Added Page tile on blog page or posts page

--- index.php
+++ index.php
@@ -9,7 +9,30 @@
 get_header(  );
 ?>
 
-<?php echo esc_html_e( "Index page", "aquila" );?>
+
+<div class="container mt-5">
+    <?php 
+        if( have_posts() ):
+            //showing page tile on exclduing front-page
+            if( is_home() && ! is_front_page() ):
+    ?>
+                <header>
+                <h1><?php  single_post_title(  )?></h1>
+                </header>
+            
+    <?php
+            endif;
+        
+        
+            while( have_posts() ): the_post(  );
+                the_title(  );
+                the_excerpt();
+                echo "<br>";
+            
+            endwhile;
+            endif;
+    ?>
+</div>
 
 <?php
     get_footer();
